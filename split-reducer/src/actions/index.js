14f6571f6fbe8fd360a2fbcd68cac045994153a6
oxape <wxdeve@126.com>
2017-03-04 14:43:04 +0800

    使用export在index.js导出使得文件结构易理解

--- split-reducer/src/actions/index.js
+++ split-reducer/src/actions/index.js
@@ -1,8 +1,4 @@
 /**
  * Created by oxape on 2017/3/3.
  */
-import * as types from '../constants/ActionTypes'
-
-export const addTodo = text => ({type: types.ADD_TODO, text})
-export const toggleTodo = id => ({type: types.TOGGLE_TODO, id})
-export const setVisibilityFilter = filter => ({type: types.SET_VISIBILITY_FILTER, filter})
+export * from './TodoActions'
