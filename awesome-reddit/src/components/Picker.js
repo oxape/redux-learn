fb49bd4de4dbfdbdb0b0f73d2626a01620df5907
oxape <wxdeve@126.com>
2017-03-07 15:02:32 +0800

    未完成

--- awesome-reddit/src/components/Picker.js
+++ awesome-reddit/src/components/Picker.js
@@ -1,21 +1,65 @@
 /**
  * Created by oxape on 2017/3/7.
  */
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
+import AppBar from 'material-ui/AppBar';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Popover from 'material-ui/Popover'
 
-const Picker = ({value, onChange, options}) => (
-    <span>
-        <h1>{value}</h1>
-        <select value={value} onChange={e => onChange(e.target.value)}>
-        {
-            options.map((option) => (
-                <option value={option} key={option}>{option}</option>
-            ))
-        }
-        </select>
-    </span>
+class Picker extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            anchorOrigin: {
+                horizontal: 'right',
+                vertical: 'bottom',
+            },
+            targetOrigin: {
+                horizontal: 'right',
+                vertical: 'bottom',
+            },
+        };
+    }
 
-)
+    render() {
+        const {value, onChange, options} = this.props
+        return (
+            <div>
+                <AppBar
+                    title={value}
+                    onLeftIconButtonTouchTap={() => {this.setState({
+                        open:!this.state.open
+                    })}}
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                >
+                    <Popover
+                        open={this.state.open}
+                        anchorEl={this.state.anchorEl}
+                        anchorOrigin={this.state.anchorOrigin}
+                        targetOrigin={this.state.targetOrigin}
+                        onRequestClose={this.handleRequestClose}
+                    >
+                        <Menu onItemTouchTap={
+                            (e, item, index) => {
+                                console.log(index)
+                                onChange(options[index])
+                                this.setState({...this.state, open:!this.state.open})
+                            }
+                        }>
+                            {
+                                options.map((option) => (
+                                    <MenuItem primaryText={option} key={option}/>
+                                ))
+                            }
+                        </Menu>
+                    </Popover>
+                </AppBar>
+            </div>
+        )
+    }
+}
 
 Picker.propTypes = {
     value: PropTypes.string.isRequired,
--- awesome-reddit/src/index.js
+++ awesome-reddit/src/index.js
@@ -5,7 +5,16 @@ import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from './containers'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
-ReactDOM.render(<Root/>, document.getElementById('root'))
+ReactDOM.render(
+    <MuiThemeProvider>
+        <Root/>
+    </MuiThemeProvider>,
+    document.getElementById('root'))
 
